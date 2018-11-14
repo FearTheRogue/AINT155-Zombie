7ed4e3495fdb60baf6a751b2f1cb773864b53b1c
FearTheRogue <43548013+FearTheRogue@users.noreply.github.com>
2018-11-14 16:19:06 +0000

    Added GameUI Script

---
+++ Zombie Shooter Project 1/Assets/Scripts/GameUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour {
+
+    public Slider healthBar;
+    public Text scoreText;
+
+    public int playerScore = 0;
+
+    private void OnEnable()
+    {
+        Player.OnHealthUpdate += UpdateHealthBar;
+        AddScore.OnSendScore += UpdateScore;
+    }
+
+    private void OnDisable()
+    {
+        Player.OnHealthUpdate -= UpdateHealthBar;
+        AddScore.OnSendScore -= UpdateScore;
+    }
+
+    private void UpdateHealthBar(int health)
+    {
+        healthBar.value = health;
+    }
+
+    private void UpdateScore(int theScore)
+    {
+        playerScore += theScore;
+        scoreText.text = "SCORE: " + theScore.ToString();
+    }
+}
