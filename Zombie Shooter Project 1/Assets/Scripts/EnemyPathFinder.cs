2e75868c091cec16ab6f596a1661fa54a5a2bdd0
FearTheRogue <43548013+FearTheRogue@users.noreply.github.com>
2018-11-14 19:00:45 +0000

    Added PathFinding to Zombies

---
+++ Zombie Shooter Project 1/Assets/Scripts/EnemyPathFinder.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyPathFinder : MonoBehaviour {
+
+    public Transform target;
+    private IAstarAI ai;
+
+	// Use this for initialization
+	private void Start () {
+        ai = GetComponent<IAstarAI>();
+	}
+
+	// Update is called once per frame
+	private void Update () {
+		if (target != null && ai != null)
+        {
+            ai.destination = target.position;
+            ai.SearchPath();
+        }
+	}
+
+    public void SetTarget(Transform newTarget)
+    {
+        target = newTarget;
+    }
+} // EnemyPathFinder
